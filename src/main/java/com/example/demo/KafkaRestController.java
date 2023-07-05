8554de9debd4042867d71a57c8a2b43b86270fa7
Atakan Bayrak <atakanbayrak461@hotmail.com>
2023-07-06 01:41:08 +0300

    kafka start up

---
+++ src/main/java/com/example/demo/KafkaRestController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaRestController {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaRestController(KafkaTemplate<String,String> kafkaTemplate){
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @GetMapping("/produce/{message}")
+    public String ProduceMessage(@PathVariable String message){
+        kafkaTemplate.send("example-topic", message);
+        return "Message sent: " + message;
+    }
+
+    @KafkaListener(topics = "example-topic")
+    public void ConsumeMessage(String message){
+        System.out.println("Received Message :" + message);
+    }
+}
