72c27cfccad58954d6d5932a7fbe75dcf051a7dd
Atakan Bayrak <atakanbayrak461@hotmail.com>
2023-07-06 01:50:46 +0300

    kafka building up

--- src/main/java/com/example/demo/KafkaRestController.java
+++ src/main/java/com/example/demo/KafkaRestController.java
@@ -22,7 +22,7 @@ public class KafkaRestController {
         kafkaTemplate.send("example-topic", message);
         return "Message sent: " + message;
     }
-// new changes updated
+// new changes updated recently
     @KafkaListener(topics = "example-topic")
     public void ConsumeMessage(String message){
         System.out.println("Received Message :" + message);
